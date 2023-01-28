8b49173117f13f9208d21b1a7b41debdb89a5249
Mischievous07 <123273279+Mischievous07@users.noreply.github.com>
2023-01-28 17:38:36 -0500

    Update Lab21a.java

--- Lab21a.java
+++ Lab21a.java
@@ -1,24 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+var miles = prompt("Input amount of Miles:" )
+var kilometers = miles * 1.609;
 
-package com.mycompany.lab2.a;
-
-/**
- *
- * @author hkrav
- */
-import java.util.Scanner;
-public class Lab21a {
-    public static void main(String[] args) {
-        
- {
-  Scanner converter = new Scanner(System.in);
- 
-   System.out.println("Enter amount of miles");
-      double miles = converter.nextDouble();
-       double conversion = miles * 1.609;
-        System.out.println("Your miles to kilometers conversion is: " + conversion);
- }
- }
-}
+console.log("That amount of miles is" + kilometers + " kilometers");
