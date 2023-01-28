a0341bcf8d25eb11e1315c1a64a6323b8c0ff0d6
Mischievous07 <123273279+Mischievous07@users.noreply.github.com>
2023-01-28 17:48:33 -0500

    Update Lab22java.java

--- Lab22java.java
+++ Lab22java.java
@@ -1,38 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+let inches = prompt ("Enter height in inches");
+let centimeters = inches * 2.54;
+let lbs = prompt ("Enter weight in pounds");
+let kilos = lbs / 2.205;
 
-/**
- *
- * @author hkrav
- */import java.util.Scanner;
-
-public class Lab22java {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       
-        
-         
-        int ineedthis = 703;
-        
-        Scanner calculator = new Scanner(System.in);
-        
-        System.out.println("Please enter your height in inches: ");
-        double heightInches = calculator.nextDouble();
-        System.out.println("Please enter your weight in pounds: ");
-         double weight = calculator.nextDouble();
-         
-             double weightKilo = weight / 2.205;
-                double heightCentimeter = heightInches * 2.54;
-                  double BMI = ineedthis * weight / (heightInches * heightInches);
-       System.out.println("Your weight in kilograms is: " + weightKilo);
-        System.out.println("Your height in centimeters is: " + heightCentimeter);
-        
-         System.out.println("That brings your Body Mass Index to: " + BMI);
-    }
-    
-}
+ let bmi = (kilos / ((centimeters * centimeters) 
+                            / 10000)).toFixed(2);
+ console.log("Height in centimeters is: " + centimeters);
+ console.log("Weight in kilos is : " + kilos);
+console.log("Your BMI is: " + bmi);
