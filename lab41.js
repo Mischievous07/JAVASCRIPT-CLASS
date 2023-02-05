df735c815cbfdd62b58b72da379c8a50c404499f
Mischievous07 <123273279+Mischievous07@users.noreply.github.com>
2023-02-05 17:26:00 -0500

    Create lab41.js

---
+++ lab41.js
@@ -0,0 +1,13 @@
+let randomnumber = Math.random();
+let inputNum = prompt ("Input your number");
+
+if (inputNum > randomnumber) {
+console.log ("Your number is higher!")
+
+}
+else if (inputNum < randomnumber) {
+console.log("Your number is lower!");
+}
+else {
+console.log("Your number is equal!");
+}
